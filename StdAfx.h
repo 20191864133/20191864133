6b27ed5b45d9e0ad29ca210b51843e215138d040
20191864133 <2538262357@qq.com>
2019-10-11 18:43:34 +0800

    first commit

---
+++ StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__A7A9A020_923C_44FA_9EFF_4B0CD90D8822__INCLUDED_)
+#define AFX_STDAFX_H__A7A9A020_923C_44FA_9EFF_4B0CD90D8822__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__A7A9A020_923C_44FA_9EFF_4B0CD90D8822__INCLUDED_)
---
+++ tenstars.cpp
@@ -0,0 +1,15 @@
+// tenstars.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	int i=0;
+	for(i=0;i<10;i++)
+	{
+		printf("**********\n");
+	}
+	return 0;
+}
+
